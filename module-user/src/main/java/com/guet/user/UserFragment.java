d982d4bc50ba761ca15728f317fd239ec27f097a
notes_514 <851559442@qq.com>
2022-01-08 10:50:58 +0800

    新增用户个人中心页UI

--- module-user/src/main/java/com/guet/user/UserFragment.java
+++ module-user/src/main/java/com/guet/user/UserFragment.java
@@ -1,42 +1,32 @@
 package com.guet.user;
 
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
+import com.blankj.utilcode.util.ToastUtils;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.bitmap.CircleCrop;
 import com.bumptech.glide.request.RequestOptions;
 import com.guet.base.fragment.MvvmBaseFragment;
 import com.guet.base.viewmodel.IMvvmBaseViewModel;
 import com.guet.common.router.RouterFragmentPath;
-import com.guet.user.adapter.RecyclerAdapter;
 import com.guet.user.databinding.UserFragmentLayoutBinding;
 import com.guet.user.login.LoginActivity;
-
-import java.util.ArrayList;
-import java.util.List;
+import com.guet.user.view.DLAnimView;
 
 /**
- * 应用模块: 用户模块
- * <p>
- * 类描述:
- * <p>
+ * 用户个人中心
  *
- * @author darryrzhoong
- * @since 2020-02-28
+ * @author dhxstart
+ * @date 2022/1/8 10:49
  */
 @Route(path = RouterFragmentPath.User.PAGER_USER)
 public class UserFragment extends MvvmBaseFragment<UserFragmentLayoutBinding, IMvvmBaseViewModel> {
-
-    private RecyclerAdapter adapter;
 
     @Override
     public int getLayoutId() {
@@ -51,46 +41,53 @@ public class UserFragment extends MvvmBaseFragment<UserFragmentLayoutBinding, IM
     }
 
     private void initData() {
-        List<String> items = new ArrayList<>();
-        items.add("我的关注");
-        items.add("我的收藏");
-        items.add("视频功能声明");
-        items.add("用户协议");
-        items.add("版权声明");
-        items.add("关于作者");
-        adapter.setNewData(items);
     }
 
     private void initView() {
         if (getActivity() == null) {
             return;
         }
+        // 设置登录背景图片
+        DLAnimView dlAnimView = new DLAnimView(getActivity());
+        ConstraintLayout.LayoutParams layoutParams =
+                new ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.MATCH_PARENT, 128);
+        viewDataBinding.userConstraint.addView(dlAnimView, layoutParams);
+
         Glide.with(getActivity()).load(getContext().getDrawable(R.drawable.avatar))
                 .apply(RequestOptions.bitmapTransform(new CircleCrop()))
                 .into(viewDataBinding.ivAvatar);
-        viewDataBinding.rvTables.setHasFixedSize(true);
-        viewDataBinding.rvTables
-                .setLayoutManager(new LinearLayoutManager(getContext()));
-        adapter = new RecyclerAdapter();
-        adapter.setFooterView(getFooterView());
-        viewDataBinding.rvTables.setAdapter(adapter);
-        viewDataBinding.ivMore.setOnClickListener(v -> {
-            LoginActivity.startAction(getActivity());
-        });
-        viewDataBinding.tvLike.setOnClickListener(v -> {
-            LoginActivity.startAction(getActivity());
-        });
-        viewDataBinding.tvReply.setOnClickListener(v -> {
-            LoginActivity.startAction(getActivity());
-        });
-        viewDataBinding.ivAvatar.setOnClickListener(v -> {
-            LoginActivity.startAction(getActivity());
-        });
-    }
 
-    private View getFooterView() {
-        return LayoutInflater.from(getContext())
-                .inflate(R.layout.user_item_footer_view, viewDataBinding.rvTables, false);
+        viewDataBinding.ivAvatar.setOnClickListener(v -> {
+            ToastUtils.showShort("点击了头像");
+            LoginActivity.startAction(getActivity());
+        });
+        viewDataBinding.clTrade.setOnClickListener(v -> {
+            ToastUtils.showShort("点击了我的交易");
+        });
+        viewDataBinding.clRepair.setOnClickListener(v -> {
+            ToastUtils.showShort("点击了我的报修");
+        });
+        viewDataBinding.llProperty.setOnClickListener(v -> {
+            ToastUtils.showShort("点击了物业");
+        });
+        viewDataBinding.llBill.setOnClickListener(v -> {
+            ToastUtils.showShort("点击账单");
+        });
+        viewDataBinding.llMsgManager.setOnClickListener(v -> {
+            ToastUtils.showShort("点击了消息管理");
+        });
+        viewDataBinding.llComplaint.setOnClickListener(v -> {
+            ToastUtils.showShort("点击了公告管理");
+        });
+        viewDataBinding.llFeedBack.setOnClickListener(v -> {
+            ToastUtils.showShort("点击了意见反馈");
+        });
+        viewDataBinding.llAbout.setOnClickListener(v -> {
+            ToastUtils.showShort("点击了关于");
+        });
+        viewDataBinding.llSetting.setOnClickListener(v -> {
+            ToastUtils.showShort("点击了设置");
+        });
     }
 
     @Override
